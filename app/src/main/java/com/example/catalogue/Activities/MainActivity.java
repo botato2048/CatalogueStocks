d154aa4bec7cdccaf309bc5df1949f29db833be5
botato2048 <ema@tfs.ca>
2023-01-09 09:09:57 -0500

    jan9

--- app/src/main/java/com/example/catalogue/Activities/MainActivity.java
+++ app/src/main/java/com/example/catalogue/Activities/MainActivity.java
@@ -5,10 +5,13 @@ import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.util.Log;
 
 import com.android.volley.RequestQueue;
 
 import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonArrayRequest;
 import com.android.volley.toolbox.JsonObjectRequest;
 import com.android.volley.toolbox.Volley;
 import com.example.catalogue.Data.recycleViewAdapter;
@@ -16,6 +19,7 @@ import com.example.catalogue.Model.StockMarket;
 import com.example.catalogue.R;
 import com.example.catalogue.Util.prefs;
 
+import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
@@ -52,7 +56,21 @@ public class MainActivity extends AppCompatActivity {
         StockList.clear();
 
         String myurl = "https://quotient.p.rapidapi.com/equity/intraday?symbol=" + searchTerm + "&interval=1&from=2022-01-01 10:00&to=2022-12-25 10:00&adjust=false";
-        JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(myurl, new Response.Listener<JSONObject>() {
+        JsonArrayRequest jsonArrayRequest=new JsonArrayRequest(myurl, new Response.Listener<JSONArray>() {
+            @Override
+            public void onResponse(JSONArray response)
+            {
+                Log.d("JSON","message=" + response);
+
+
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+
+            }
+        });
+     /*   JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(myurl, new Response.Listener<JSONObject>() {
             @Override
             public void onResponse(JSONObject response) {
                 try {
@@ -62,7 +80,7 @@ public class MainActivity extends AppCompatActivity {
                 }
             }
         })
-
-
+*/
+return  StockList;
     }
 }
